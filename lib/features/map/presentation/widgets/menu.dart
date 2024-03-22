6de71ca5b6e1947f358bd6190fb013a29157bc58
ishaf <shafiulislam20@gmail.com>
2024-03-23 02:28:43 +0600

    feat: dummy menu

--- lib/features/map/presentation/widgets/menu.dart
+++ lib/features/map/presentation/widgets/menu.dart
@@ -1,4 +1,6 @@
+import 'package:bari_koi_map_with_autocomplete/core/config/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AppMenu extends StatelessWidget {
   const AppMenu({super.key});
@@ -10,8 +12,60 @@ class AppMenu extends StatelessWidget {
       body: Column(
         children: [
           Container(
-            height: 56,
-            color: Colors.red,
+            height: 180.h,
+            width: MediaQuery.sizeOf(context).width,
+            color: AppColor.primary,
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16).w,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 50.h,
+                      width: 50.h,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 16.h,
+                    ),
+                    Text(
+                      'Bari Koi??',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 21.r,
+                      ),
+                    ),
+
+                    //email
+                    const Text(
+                      'GwOzK@example.com',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: const Icon(Icons.person),
+            title: const Text('Profile'),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: const Icon(Icons.settings),
+            title: const Text('Settings'),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: const Icon(Icons.logout),
+            title: const Text('Logout'),
           ),
         ],
       ),
